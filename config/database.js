e4f12e3d187a40c77dab9899688a88a3fe449121
Finn Walker <finn3walker@gmail.com>
2019-08-16 16:36:20 +0100

    first commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// Set up mongoose connection
+const mongoose = require('mongoose');
+const mongoDB = 'mongodb://localhost/RC_API';
+mongoose.connect(mongoDB, {useNewUrlParser: true});
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
