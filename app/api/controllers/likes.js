0fdcb7a4656a63c98821662d3c87b7c619bd28f2
FinnWalker <finn3walker@gmail.com>
2019-08-19 02:28:48 +0100

    Fixed json

--- app/api/controllers/likes.js
+++ app/api/controllers/likes.js
@@ -27,7 +27,7 @@ module.exports = {
                         if(err) {
                             next(err);
                         }else{
-                            res.json("Like removed");
+                            res.json({message: "Like removed"});
                         }
                     });
                 }else{
