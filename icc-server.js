60d8971298bedd383f3e7ba9646a791ea46d7d26
FinnWalker <finn3walker@gmail.com>
2019-11-20 03:10:37 +1100

    test

--- icc-server.js
+++ icc-server.js
@@ -1,27 +1,31 @@
-const express = require('express');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
 
-const mongoose = require('./config/database'); // Database configuration
+const mongoose = require("./config/database"); // Database configuration
 
 const app = express();
 app.use(cors());
 
-mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "MongoDB connection error:")
+);
 
-app.use(logger('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const usersController = require("./app/api/controllers/users.js");
 
-app.post('/icc/users/register', usersController.create);
+app.post("/icc/users/register", usersController.create);
 //app.get('/icc/users/user_data', userController.getData);
-app.post('/icc/users/user_data', usersController.getData);
-app.get('/icc/users/user_data', (req, res) => {
-    res.sendFile(path.join(
-        __dirname,
-        "data.html"));
+app.post("/icc/users/user_data", usersController.getData);
+app.get("/icc/users/user_data", (req, res) => {
+  res.sendFile(path.join(__dirname, "data.html"));
 });
 
-app.listen(6000, () => {console.log('Server listening on port 6000')});
+app.listen(6000, () => {
+  console.log("Server listening on port 6000");
+});
